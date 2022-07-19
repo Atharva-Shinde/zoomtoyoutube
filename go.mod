2fa8897816ed96e667424e86e7a795b91563c2fd
Atharva Shinde <atharvashinde179@gmail.com>
2022-07-19 23:37:47 +0530

    created: file

---
+++ go.mod
@@ -0,0 +1,3 @@
+module zoomtoyoutube.com/main
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+	postrecording()
+	storerecording()
+}
+
+func storerecording() {
+	panic("unimplemented")
+}
+
+func postrecording() {
+	panic("unimplemented")
+}
