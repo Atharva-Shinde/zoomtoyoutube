5428c48d213b5a693f8ca15bba8e2368f5dcccfb
Atharva Shinde <atharvashinde179@gmail.com>
2022-11-24 00:10:56 +0530

    initiate: fetching zcloud recording

--- getrecording.go
+++ getrecording.go
@@ -10,10 +10,15 @@ type (
 		Status      string `json:status`
 	}
 
-	file struct {
-		MeetingID     string `json:meetingid`
-		UUID          string `json:uuid`
-		IncludeFields string `json:include_fields`
-		AccountID     string `json:account_id`
+	Meeting struct {
+		AccountID      string          `json:account_id`
+		HostID         string          `json:host_id`
+		ID             string          `json:id`
+		IncludeFields  string          `json:include_fields`
+		Password       string          `json:password` // cloud recording password
+		RecordingCount string          `json:recording_count`
+		RecordingFiles []RecordingFile `json:recording_files`
+		Topic          string          `json:topic`
+		UUID           string          `json:uuid`
 	}
 )
